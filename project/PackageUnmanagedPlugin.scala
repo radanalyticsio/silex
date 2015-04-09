a36445106a90858d9db6a7d88613980d8bfcb61d
Erik Erlandson <eerlands@redhat.com>
2015-04-09 11:41:45 -0700

    unmanagedJarDir

--- project/PackageUnmanagedPlugin.scala
+++ project/PackageUnmanagedPlugin.scala
@@ -2,12 +2,12 @@ import sbt._
 import Keys._
 
 object packageUnmanaged extends Plugin {
-  lazy val unmanagedPackageDir = SettingKey[java.io.File]("directory used by packageUnmanage") 
+  lazy val unmanagedJarDir = SettingKey[java.io.File]("directory used by packageUnmanage task") 
   lazy val packageUnmanaged = TaskKey[java.io.File]("packageUnmanaged", "Package jars to unmanaged jar directory")
 
   override val settings = Seq(
-    unmanagedPackageDir := file(Path.userHome.absolutePath) / "sbtUnmanagedJars",
-    packageUnmanaged <<= (packageBin in Compile, unmanagedPackageDir) map { (path, upath) =>
+    unmanagedJarDir := file(Path.userHome.absolutePath) / "sbtUnmanagedJars",
+    packageUnmanaged <<= (packageBin in Compile, unmanagedJarDir) map { (path, upath) =>
       {
         (s"mkdir -p $upath").!
         (s"cp -f $path $upath").!
