bbb3a36224f226c9c0f5702a2d495a35ee2a181b
Erik Erlandson <eerlands@redhat.com>
2015-04-17 12:54:01 -0700

    doc for FeatureSeq

--- src/main/scala/com/redhat/et/silex/feature/extractor/extractor.scala
+++ src/main/scala/com/redhat/et/silex/feature/extractor/extractor.scala
@@ -16,25 +16,81 @@
  * limitations under the License.c
  */
 
+/** Provides a feature extraction type system that supports concatenation of feature extraction
+  * functions in a logically constant and type safe manner.  Extraction functions are represented by
+  * instances of [[com.redhat.et.silex.feature.extractor.Extractor]].
+  */
 package com.redhat.et.silex.feature.extractor
 
 import com.redhat.et.silex.feature.indexfunction._
 
+/** An immutable sequence of Double values representing feature vectors extracted by an
+  * [[Extractor]].  [[FeatureSeq]] is a monoid with respect to the concatenation operator [[++]].
+  */
 abstract class FeatureSeq extends scala.collection.immutable.Seq[Double] with Serializable {
+  /** The length of the feature sequence
+    *
+    * @return The number of feature values in the sequence
+    */
   def length: Int
+
+  /** The jth element of the feature sequence
+    *
+    * @param j element index
+    * @return The value of jth feature value
+    */
   def apply(j: Int): Double
+
+  /** Obtain a new iterator over the elements of the feature sequence
+    *
+    * @return An iterator over the feature values
+    */
   def iterator: Iterator[Double]
+
+  /** Obtain a new iterator over indices of explicitly-stored feature values.  Equivalent
+    * to [[keysIterator]] when the feature sequence is dense.
+    *
+    * @return An iterator over indices of explicitly-stored feature values.
+    */
   def activeKeysIterator: Iterator[Int]
+
+  /** Obtain a new iterator over explicitly-stored feature values.  Equivalent to [[valuesIterator]]
+    * when the feature sequence is dense.
+    *
+    * @return An iterator over explicitly-stored values
+    */
   def activeValuesIterator: Iterator[Double]
 
+  /** The density of explicitly-stored feature values. Defined as the ratio of explicitly-stored
+    * values to total [[length]].
+    *
+    * @return The density of explicitly-stored values
+    */
   def density: Double
 
+  /** Concatenate two feature sequences.
+    *
+    * @param that The right-argument.  Feature values are appended to the result.
+    * @return A new feature sequence consisting of left-argument followed by the right-argument.
+    */
   final def ++(that: FeatureSeq): FeatureSeq = new ConcatFS(this, that)
 
+  /** Obtain a new iterator over the indices of the feature sequence.
+    *
+    * @return An iterator over all the indices defined by the feature sequence.
+    */
   final def keysIterator = Iterator.range(0, length)
+
+  /** Obtain a new iterator over the feature values in a feature sequence.
+    *
+    * @return An iterator over all feature values in the sequence.
+    */
   final def valuesIterator = iterator
 }
 
+/** Subclass of [[FeatureSeq]] representing the concatenation of two feature sequences.  The
+  * type returned by [[++]] operator
+  */
 sealed class ConcatFS(fb1: FeatureSeq, fb2: FeatureSeq) extends FeatureSeq {
   def length = length_
   private lazy val length_ = fb1.length + fb2.length
@@ -54,6 +110,7 @@ sealed class ConcatFS(fb1: FeatureSeq, fb2: FeatureSeq) extends FeatureSeq {
   override def toString = s"ConcatFS(${fb1}, ${fb2})"
 }
 
+/** Subclass of [[FeatureSeq]] representing a single Scala [[Seq]] object. */
 sealed class SeqFS(seq: Seq[Double]) extends FeatureSeq {
   def length = seq.length
   def apply(j: Int) = seq(j)
@@ -64,10 +121,15 @@ sealed class SeqFS(seq: Seq[Double]) extends FeatureSeq {
   override def toString = s"SeqFS(${seq})"
 }
 
+/** Provides factory methods for [[FeatureSeq]] instances. */
 object FeatureSeq {
   import scala.language.implicitConversions
 
-  def empty = new FeatureSeq {
+  /** Obtain an empty feature sequence
+    *
+    * @return a feature sequence of length zero.
+    */
+  def empty: FeatureSeq = new FeatureSeq {
     def length = 0
     def apply(j: Int) = (Seq.empty)(j)
     def iterator = Iterator.empty
@@ -80,7 +142,19 @@ object FeatureSeq {
   implicit def fromArrayToSeqFS(a: Array[Double]): FeatureSeq =
     new SeqFS(a:scala.collection.mutable.WrappedArray[Double])
 
-  def apply[T <% FeatureSeq](x: T) = x:FeatureSeq
+  /** Obtain a feature sequence from a Scala [[Seq]], [[Array]], or any other object with
+    * a defined conversion to [[FeatureSeq]].
+    *
+    * @param obj The object to create a feature sequence from
+    * @return A new feature sequence created from the argument object
+    */
+  def apply[T <% FeatureSeq](obj: T) = obj:FeatureSeq
+
+  /** Create a feature sequence from the given sequence of zero or more feature values.
+    *
+    * @param values A list of zero or more feature values to be contained in the sequence
+    * @return A new feature sequence containing the given values, in the order supplied.
+    */
   def apply(values: Double*): FeatureSeq = new SeqFS(values.toVector)
 }
 
