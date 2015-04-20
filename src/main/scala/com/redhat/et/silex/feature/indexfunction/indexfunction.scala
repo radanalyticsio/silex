c648743722da572e6b61d1b2599110c16c3c58c9
Erik Erlandson <eerlands@redhat.com>
2015-04-20 14:53:30 -0700

    factory doc

--- src/main/scala/com/redhat/et/silex/feature/indexfunction/indexfunction.scala
+++ src/main/scala/com/redhat/et/silex/feature/indexfunction/indexfunction.scala
@@ -20,16 +20,39 @@ package com.redhat.et.silex.feature.indexfunction
 
 import scala.language.implicitConversions
 
-// An index function is a (partial) function from some subset of collection indices to 
-// some values, and which can be concatenated with other such functions such that they 
-// now cover an expanded index range, and the "right" side function has had its domain
-// shifted.  An index function is defined on an index interval with a particular width,
-// and its domain is some subset of that interval
+/** An index function is a (partial) function from some subset of collection indices to 
+  * some values, and which can be concatenated with other such functions such that they 
+  * now cover an expanded index range, and the "right" side function has had its domain
+  * shifted.  An index function is defined on an index interval with a particular width,
+  * and its domain is some subset of that interval.
+  *
+  * @tparam V The type of value returned by this function.
+  */
 abstract class IndexFunction[V] extends PartialFunction[Int, V] with Serializable { self =>
+
+  /** The width of the integer index interval that contains the [[domain]].  All indices this
+    * function is defined on are on the interval [0, width)
+    *
+    * @return The width of the interval [0, width) containing the function [[domain]].
+    */
   def width: Int
+
+  /** Obtain a new iterator over the indices that the function is defined on.
+    *
+    * @return An iterator over the function domain
+    */
   def domain: Iterator[Int]
+
+  /** Obtain a new iterator over the function range values
+    *
+    * @return An iterator over values that may be returned by the function
+    */
   def range: Iterator[V]
 
+  /** Obtain a Scala Map from values in the [[domain]] to their corresponding [[range]] values.
+    *
+    * @return A Scala Map that is equivalent to this function
+    */
   final def toMap: Map[Int, V] = {
     val m = scala.collection.mutable.Map.empty[Int, V]
     domain.foreach { j =>
@@ -38,6 +61,14 @@ abstract class IndexFunction[V] extends PartialFunction[Int, V] with Serializabl
     m.toMap
   }
 
+  /** Concatenate two index functions.
+    *
+    * @param that The right-hand function of the concatenation
+    * @return The concatenation of left and right index functions, defined as:
+    * {{{
+    * (f1 ++ f2)(j) = if (j < f1.width) f1(j) else f2(j - f1.width)
+    * }}}
+    */
   final def ++(that: IndexFunction[V]) = new IndexFunction[V] {
     def width = self.width + that.width
     def domain = self.domain ++ that.domain.map(_ + self.width)
@@ -54,12 +85,36 @@ abstract class IndexFunction[V] extends PartialFunction[Int, V] with Serializabl
   }
 }
 
+/** The inverse of an [[InvertableIndexFunction]].
+  *
+  * @tparam V The [[domain]] type of this function.  Also the
+  * [[InvertableIndexFunction!.range range]] of the parent [[InvertableIndexFunction]].
+  */
 abstract class InverseIndexFunction[V] extends PartialFunction[V, Int] with Serializable {
+
+  /** The width of index range. Corresponds to the [[InvertableIndexFunction!.width width]]
+    * of the parent [[InvertableIndexFunction]].
+    */
   def width: Int
+
+  /** Obtain a new iterator over the domain of this function.
+    *
+    * @return An iterator over the domain values of this function.  Equivalent to the range of the
+    * parent [[InvertableIndexFunction]].
+    */
   def domain: Iterator[V]
 
+  /** Obtain a new iterator over the range values of this function.
+    *
+    * @return An iterator over the range values of this function.  Equivalent to the domain of
+    * the parent [[InvertableIndexFunction]].
+    */
   final def range: Iterator[Int] = domain.map(this)
 
+  /** Obtain a Scala Map that is equivalent to this function.
+    *
+    * @return A Map that maps domain values into their corresponding index values
+    */
   final def toMap: Map[V, Int] = {
     val m = scala.collection.mutable.Map.empty[V, Int]
     domain.foreach { v =>
@@ -69,11 +124,31 @@ abstract class InverseIndexFunction[V] extends PartialFunction[V, Int] with Seri
   }
 }
 
+/** An [[IndexFunction]] that is also invertable (and so 1-1).
+  *
+  * @tparam V The [[range]] value type of this function.
+  */
 abstract class InvertableIndexFunction[V] extends IndexFunction[V] { self =>
+
+  /** Obtain the inverse of this function.
+    *
+    * @return The inverse function that maps [[range]] values to their correponding indices
+    */
   def inverse: InverseIndexFunction[V]
 
   final def range = domain.map(this)
 
+  /** Concatenate two invertable index functions.
+    *
+    * @note The [[range]] values of the argument functions must be disjoint, in order to
+    * preserve the invertability of their concatenation.
+    *
+    * @param that The right-hand function of the concatenation
+    * @return The concatenation of left and right index functions, defined as:
+    * {{{
+    * (f1 ++ f2)(j) = if (j < f1.width) f1(j) else f2(j - f1.width)
+    * }}}
+    */
   final def ++(that: InvertableIndexFunction[V]) = {
     // check up front that ranges are disjoint, such that concatenation is invertable
     val iself = self.inverse
@@ -104,10 +179,20 @@ abstract class InvertableIndexFunction[V] extends IndexFunction[V] { self =>
   }
 }
 
-
+/** Factory methods for generating index functions */
 object IndexFunction {
+
+  /** Obtain an empty index function.
+    *
+    * @return An index function over the empty interval [0, 0), defined over no values
+    */
   def empty[V] = undefined[V](0)
 
+  /** Obtain an index function that is undefined over its interval
+    *
+    * @param wid The function's index interval width
+    * @return An index function that is not defined over any indices on its interval [0, wid)
+    */
   def undefined[V](wid: Int): IndexFunction[V] = {
     require(wid >= 0)
     new IndexFunction[V] {
@@ -120,6 +205,12 @@ object IndexFunction {
     }
   }
 
+  /** Obtain an index function that is constant over its interval
+    *
+    * @param v The constant value
+    * @param wid The interval width
+    * @return an index function that returns (v) over all index values [0, wid)
+    */
   def constant[V](v: V, wid: Int): IndexFunction[V] = {
     require(wid >= 0)
     new IndexFunction[V] {
@@ -132,6 +223,11 @@ object IndexFunction {
     }
   }
 
+  /** Create an index function from a Scala IndexedSeq
+    *
+    * @param vals The indexed sequence of values
+    * @return An index function over interval [0, vals.length) where f(j) = vals(j)
+    */
   def apply[V](vals: IndexedSeq[V]): IndexFunction[V] = new IndexFunction[V] {
     def width = vals.length
     def domain = (0 until width).iterator
@@ -141,8 +237,22 @@ object IndexFunction {
     def apply(j: Int) = vals(j)
   }
 
+  /** Create an index function from ordered pairs (j0, v0), (j1, v1), ...
+    *
+    * @param wid The index interval width
+    * @param pairs Zero or more pairs (j0, v0), (j1, v1), ...
+    * @return An index function with [[domain]] { j0, j1, ...} a subset of [0, wid), where
+    * f(j0) = v0, f(j1 = v1), ...  and undefined elsewhere
+    */
   def apply[V](wid: Int, pairs: (Int, V)*): IndexFunction[V] = this(wid, Map(pairs:_*))
 
+  /** Create an index function from a Scala Map of indices to values
+    *
+    * @param wid The index inverval width
+    * @param map The Scala Map
+    * @return An index function with [[domain]] equivalent to map.keysIterator, and subset of 
+    * [0, wid), where f(j) = map(j), for any j defined on the map, and undefined elsewhere
+    */
   def apply[V](wid: Int, map: Map[Int, V]): IndexFunction[V] = new IndexFunction[V] {
     require(wid >= 0)
     require(!map.keysIterator.exists(k => (k < 0  ||  k >= wid)))
@@ -156,8 +266,18 @@ object IndexFunction {
 }
 
 object InvertableIndexFunction {
+  /** Obtain an empty invertable index function.
+    *
+    * @return An invertable index function over the empty interval [0, 0), defined over no values
+    */
   def empty[V] = undefined[V](0)
 
+  /** Obtain an invertable index function that is undefined over its interval
+    *
+    * @param wid The function's index interval width
+    * @return An invertable index function that is not defined over any indices on its
+    * interval [0, wid)
+    */
   def undefined[V](wid: Int): InvertableIndexFunction[V] = {
     require(wid >= 0)
     new InvertableIndexFunction[V] {
@@ -176,8 +296,15 @@ object InvertableIndexFunction {
     }
   }
 
+  /** Create an invertable index function from a Scala IndexedSeq
+    *
+    * @param values The indexed sequence of values
+    * @return An invertable index function over interval [0, values.distinct.length) where 
+    * f(j) = values.distinct(j)
+    * @note (values) is first reduced to unique elements to ensure invertability
+    */
   def apply[V](values: IndexedSeq[V]): InvertableIndexFunction[V] = {
-    val vals: IndexedSeq[V] = values.distinct
+    val vals = values.distinct
     val v2i = v2iMap(vals)
     new InvertableIndexFunction[V] {
       def width = vals.length
@@ -194,8 +321,22 @@ object InvertableIndexFunction {
     }
   }
 
+  /** Create an invertable index function from ordered pairs (j0, v0), (j1, v1), ...
+    *
+    * @param wid The index interval width
+    * @param pairs Zero or more pairs (j0, v0), (j1, v1), ...
+    * @return An invertable index function with [[domain]] { j0, j1, ...} a subset of [0, wid),
+    * where f(j0) = v0, f(j1 = v1), ...  and undefined elsewhere
+    */
   def apply[V](wid: Int, pairs: (Int, V)*): InvertableIndexFunction[V] = this(wid, Map(pairs:_*))
 
+  /** Create an invertable index function from a Scala Map of indices to values
+    *
+    * @param wid The index inverval width
+    * @param map The Scala Map
+    * @return An invertable index function with [[domain]] equivalent to map.keysIterator, and
+    * subset of [0, wid), where f(j) = map(j), for any j defined on the map, and undefined elsewhere
+    */
   def apply[V](wid: Int, map: Map[Int, V]): InvertableIndexFunction[V] = {
     checkRange(map.valuesIterator)
     new InvertableIndexFunction[V] { self =>
