73f1f26e5369d72aa6c31d28ec7702bc00449690
Erik Erlandson <eerlands@redhat.com>
2015-06-10 10:19:48 -0700

    unit testing for iidFeatureSeqRDD

--- src/test/scala/com/redhat/et/silex/sample/iid/rdd/IIDFeatureSamplingMethodsRDD.scala
+++ src/test/scala/com/redhat/et/silex/sample/iid/rdd/IIDFeatureSamplingMethodsRDD.scala
@@ -29,37 +29,81 @@ class IIDFeatureSamplingMethodsRDDSpec extends FlatSpec with Matchers with PerTe
   import com.redhat.et.silex.sample.iid.implicits._
 
   def iidTest(data: SamplingIterator[Seq[Double]]) = {
+    var success = true
     val idxSet = (0 until data.next.length).toVector
     idxSet.combinations(2).flatMap(_.permutations).foreach { idxPair =>
       val (j, r) = (idxPair(0), idxPair(1))
-      val rvals = data.sample(1000).map(_(r)).distinct
-      val du = data.map(_(j))
+      // distinct values in column (r)
+      val rvals = data.fork.map(_(r)).sample(1000).distinct
+      // unconditioned sampling from column (j)
+      val du = data.fork.map(_(j))
+      // unconditioned sampling should have same distribution as sampling conditioned on
+      // any value from column (r), otherwise the columns aren't independent distributions
+      rvals.foreach { v =>
+        val dc = data.fork.filter(s => s(r) == v).map(_(j))
+        if (medianKSD(du, dc) > D) {
+          success = false
+        }
+      }
     }
-    true
+    success
   }
 
   scala.util.Random.setSeed(23571113)
 
-  it should "sample sequences i.i.d" in {
+  it should "sample sequences of zeros and ones i.i.d" in {
     // Data that has a joint distribution very different than i.i.d. of its marginals
-    val data = (1 to 10000).map { unused =>
-      if (scala.util.Random.nextDouble() < 0.2) Seq(0.0, 1.0) else Seq(1.0, 0.0)
+    val data = Vector.fill(10000) {
+      val t = scala.util.Random.nextDouble()
+      if      (t < 0.2) Seq(1.0, 0.0, 0.0)
+      else if (t < 0.5) Seq(0.0, 1.0, 0.0)
+      else              Seq(0.0, 0.0, 1.0)
     }
+    // Verify that columns of data are not independent
+    iidTest(SamplingIterator { data }) should be (false)
+
     val rdd = context.parallelize(data, 1)
 
     val iid = rdd.iidFeatureSeqRDD(9999, iSS = 1000, oSS = 1000).collect
     iid.length should be (9999)
 
-    medianKSD(
-      SamplingIterator { iid.map(_(0)) },
-      SamplingIterator.continually { if (scala.util.Random.nextDouble() < 0.2) 0.0 else 1.0 }
-    ) should be < D
+    // Individual marginal distributions should remain the same
+    (0 until data.head.length).foreach { j =>
+      medianKSD(
+        SamplingIterator { iid.map(_(j)) },
+        SamplingIterator { data.map(_(j)) }
+      ) should be < D
+    }
 
-    medianKSD(
-      SamplingIterator { iid.map(_(1)) },
-      SamplingIterator.continually { if (scala.util.Random.nextDouble() < 0.2) 1.0 else 0.0 }
-    ) should be < D
+    // Verify that sampled columns are statistically independent of each other
+    iidTest(SamplingIterator { iid }) should be (true)
+  }
 
+  it should "sample sequences of different values i.i.d" in {
+    // Data that has a joint distribution very different than i.i.d. of its marginals
+    val data = Vector.fill(10000) {
+      val t = scala.util.Random.nextDouble()
+      if      (t < 0.2) Seq(1.0, 0.0, 2.0)
+      else if (t < 0.5) Seq(3.0, 3.0, 7.0)
+      else              Seq(2.0, 8.0, 0.0)
+    }
+    // Verify that columns of data are not independent
+    iidTest(SamplingIterator { data }) should be (false)
+
+    val rdd = context.parallelize(data, 1)
+
+    val iid = rdd.iidFeatureSeqRDD(9999, iSS = 1000, oSS = 1000).collect
+    iid.length should be (9999)
+
+    // Individual marginal distributions should remain the same
+    (0 until data.head.length).foreach { j =>
+      medianKSD(
+        SamplingIterator { iid.map(_(j)) },
+        SamplingIterator { data.map(_(j)) }
+      ) should be < D
+    }
+
+    // Verify that sampled columns are statistically independent of each other
     iidTest(SamplingIterator { iid }) should be (true)
   }
 }
