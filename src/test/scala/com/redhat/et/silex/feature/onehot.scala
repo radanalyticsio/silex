f02bf4818631f91f113a8ceabcf51f235c841557
Erik Erlandson <eerlands@redhat.com>
2015-09-03 16:16:21 -0700

    unit test extractor categoryInfo function

--- src/test/scala/com/redhat/et/silex/feature/onehot.scala
+++ src/test/scala/com/redhat/et/silex/feature/onehot.scala
@@ -33,6 +33,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.oneHotExtractor()("d") should beEqSeq(Seq(0.0, 0.0, 0.0))
     model.oneHotExtractor().width should be (3)
     model.oneHotExtractor().names.range.toSeq should beEqSeq(Seq("v=a","v=b","v=c"))
+    model.oneHotExtractor().categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1, 2))
+    model.oneHotExtractor().categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.oneHotExtractor(undefName = "*")("a") should beEqSeq(Seq(1.0, 0.0, 0.0, 0.0))
     model.oneHotExtractor(undefName = "*")("b") should beEqSeq(Seq(0.0, 1.0, 0.0, 0.0))
@@ -41,36 +43,48 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.oneHotExtractor(undefName = "*").width should be (4)
     model.oneHotExtractor(undefName = "*").names.range.toSeq should beEqSeq(
       Seq("v=a","v=b","v=c","v=*"))
+    model.oneHotExtractor(undefName = "*").categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1, 2, 3))
+    model.oneHotExtractor(undefName = "*").categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.oneHotExtractor(minFreq = 2)("a") should beEqSeq(Seq(1.0, 0.0))
     model.oneHotExtractor(minFreq = 2)("b") should beEqSeq(Seq(0.0, 1.0))
     model.oneHotExtractor(minFreq = 2)("c") should beEqSeq(Seq(0.0, 0.0))
     model.oneHotExtractor(minFreq = 2).width should be (2)
     model.oneHotExtractor(minFreq = 2).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.oneHotExtractor(minFreq = 2).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.oneHotExtractor(minFreq = 2).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.oneHotExtractor(maxFreq = 2)("a") should beEqSeq(Seq(0.0, 0.0))
     model.oneHotExtractor(maxFreq = 2)("b") should beEqSeq(Seq(1.0, 0.0))
     model.oneHotExtractor(maxFreq = 2)("c") should beEqSeq(Seq(0.0, 1.0))
     model.oneHotExtractor(maxFreq = 2).width should be (2)
     model.oneHotExtractor(maxFreq = 2).names.range.toSeq should beEqSeq(Seq("v=b","v=c"))
+    model.oneHotExtractor(maxFreq = 2).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.oneHotExtractor(maxFreq = 2).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.oneHotExtractor(minProb = 0.33)("a") should beEqSeq(Seq(1.0, 0.0))
     model.oneHotExtractor(minProb = 0.33)("b") should beEqSeq(Seq(0.0, 1.0))
     model.oneHotExtractor(minProb = 0.33)("c") should beEqSeq(Seq(0.0, 0.0))
     model.oneHotExtractor(minProb = 0.33).width should be (2)
     model.oneHotExtractor(minProb = 0.33).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.oneHotExtractor(minProb = 0.33).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.oneHotExtractor(minProb = 0.33).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.oneHotExtractor(maxProb = 0.34)("a") should beEqSeq(Seq(0.0, 0.0))
     model.oneHotExtractor(maxProb = 0.34)("b") should beEqSeq(Seq(1.0, 0.0))
     model.oneHotExtractor(maxProb = 0.34)("c") should beEqSeq(Seq(0.0, 1.0))
     model.oneHotExtractor(maxProb = 0.34).width should be (2)
     model.oneHotExtractor(maxProb = 0.34).names.range.toSeq should beEqSeq(Seq("v=b","v=c"))
+    model.oneHotExtractor(maxProb = 0.34).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.oneHotExtractor(maxProb = 0.34).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.oneHotExtractor(maxSize = 2)("a") should beEqSeq(Seq(1.0, 0.0))
     model.oneHotExtractor(maxSize = 2)("b") should beEqSeq(Seq(0.0, 1.0))
     model.oneHotExtractor(maxSize = 2)("c") should beEqSeq(Seq(0.0, 0.0))
     model.oneHotExtractor(maxSize = 2).width should be (2)
     model.oneHotExtractor(maxSize = 2).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.oneHotExtractor(maxSize = 2).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.oneHotExtractor(maxSize = 2).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.oneHotExtractor(minFreq = 4)("a") should beEqSeq(Seq.empty[Double])
     model.oneHotExtractor(maxFreq = 0)("a") should beEqSeq(Seq.empty[Double])
@@ -79,6 +93,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.oneHotExtractor(maxSize = 0)("a") should beEqSeq(Seq.empty[Double])
     model.oneHotExtractor(maxSize = 0).width should be (0)
     model.oneHotExtractor(maxSize = 0).names.range.toSeq should beEqSeq(Seq[String]())
+    model.oneHotExtractor(maxSize = 0).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.oneHotExtractor(maxSize = 0).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
   }
 
   it should "provide multiHotExtractor" in {
@@ -92,6 +108,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor()(Seq("b", "d")) should beEqSeq(Seq(0.0, 1.0, 0.0))
     model.multiHotExtractor().width should be (3)
     model.multiHotExtractor().names.range.toSeq should beEqSeq(Seq("v=a","v=b","v=c"))
+    model.multiHotExtractor().categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1, 2))
+    model.multiHotExtractor().categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.multiHotExtractor(undefName = "*")(Seq("a")) should beEqSeq(Seq(1.0, 0.0, 0.0, 0.0))
     model.multiHotExtractor(undefName = "*")(Seq("b")) should beEqSeq(Seq(0.0, 1.0, 0.0, 0.0))
@@ -102,6 +120,9 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor(undefName = "*").width should be (4)
     model.multiHotExtractor(undefName = "*").names.range.toSeq should beEqSeq(
       Seq("v=a","v=b","v=c","v=*"))
+    model.multiHotExtractor(undefName = "*").categoryInfo.domain.toSeq should beEqSeq(
+      Seq(0, 1, 2, 3))
+    model.multiHotExtractor(undefName = "*").categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.multiHotExtractor(minFreq = 2)(Seq("a")) should beEqSeq(Seq(1.0, 0.0))
     model.multiHotExtractor(minFreq = 2)(Seq("b")) should beEqSeq(Seq(0.0, 1.0))
@@ -109,6 +130,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor(minFreq = 2)(Seq("a", "c")) should beEqSeq(Seq(1.0, 0.0))
     model.multiHotExtractor(minFreq = 2).width should be (2)
     model.multiHotExtractor(minFreq = 2).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.multiHotExtractor(minFreq = 2).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.multiHotExtractor(minFreq = 2).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.multiHotExtractor(maxFreq = 2)(Seq("a")) should beEqSeq(Seq(0.0, 0.0))
     model.multiHotExtractor(maxFreq = 2)(Seq("b")) should beEqSeq(Seq(1.0, 0.0))
@@ -116,6 +139,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor(maxFreq = 2)(Seq("a", "c")) should beEqSeq(Seq(0.0, 1.0))
     model.multiHotExtractor(maxFreq = 2).width should be (2)
     model.multiHotExtractor(maxFreq = 2).names.range.toSeq should beEqSeq(Seq("v=b","v=c"))
+    model.multiHotExtractor(maxFreq = 2).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.multiHotExtractor(maxFreq = 2).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.multiHotExtractor(minProb = 0.33)(Seq("a")) should beEqSeq(Seq(1.0, 0.0))
     model.multiHotExtractor(minProb = 0.33)(Seq("b")) should beEqSeq(Seq(0.0, 1.0))
@@ -123,6 +148,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor(minProb = 0.33)(Seq("a", "c")) should beEqSeq(Seq(1.0, 0.0))
     model.multiHotExtractor(minProb = 0.33).width should be (2)
     model.multiHotExtractor(minProb = 0.33).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.multiHotExtractor(minProb = 0.33).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.multiHotExtractor(minProb = 0.33).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.multiHotExtractor(maxProb = 0.34)(Seq("a")) should beEqSeq(Seq(0.0, 0.0))
     model.multiHotExtractor(maxProb = 0.34)(Seq("b")) should beEqSeq(Seq(1.0, 0.0))
@@ -130,6 +157,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor(maxProb = 0.34)(Seq("a", "c")) should beEqSeq(Seq(0.0, 1.0))
     model.multiHotExtractor(maxProb = 0.34).width should be (2)
     model.multiHotExtractor(maxProb = 0.34).names.range.toSeq should beEqSeq(Seq("v=b","v=c"))
+    model.multiHotExtractor(maxProb = 0.34).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.multiHotExtractor(maxProb = 0.34).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.multiHotExtractor(maxSize = 2)(Seq("a")) should beEqSeq(Seq(1.0, 0.0))
     model.multiHotExtractor(maxSize = 2)(Seq("b")) should beEqSeq(Seq(0.0, 1.0))
@@ -137,6 +166,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor(maxSize = 2)(Seq("a", "c")) should beEqSeq(Seq(1.0, 0.0))
     model.multiHotExtractor(maxSize = 2).width should be (2)
     model.multiHotExtractor(maxSize = 2).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.multiHotExtractor(maxSize = 2).categoryInfo.domain.toSeq should beEqSeq(Seq(0, 1))
+    model.multiHotExtractor(maxSize = 2).categoryInfo.range.toSeq should beEqSeq(Seq(2))
 
     model.multiHotExtractor(minFreq = 4)(Seq("a")) should beEqSeq(Seq.empty[Double])
     model.multiHotExtractor(maxFreq = 0)(Seq("a")) should beEqSeq(Seq.empty[Double])
@@ -145,6 +176,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.multiHotExtractor(maxSize = 0)(Seq("a")) should beEqSeq(Seq.empty[Double])
     model.multiHotExtractor(maxSize = 0).width should be (0)
     model.multiHotExtractor(maxSize = 0).names.range.toSeq should beEqSeq(Seq[String]())
+    model.multiHotExtractor(maxSize = 0).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.multiHotExtractor(maxSize = 0).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
   }
 
   it should "provide histExtractor" in {
@@ -160,6 +193,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor()(Seq("a", "b", "a", "b", "a")) should beEqSeq(Seq(3.0, 2.0, 0.0))
     model.histExtractor().width should be (3)
     model.histExtractor().names.range.toSeq should beEqSeq(Seq("v=a","v=b","v=c"))
+    model.histExtractor().categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor().categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
 
     model.histExtractor(undefName = "*")(Seq("a")) should beEqSeq(Seq(1.0, 0.0, 0.0, 0.0))
     model.histExtractor(undefName = "*")(Seq("b")) should beEqSeq(Seq(0.0, 1.0, 0.0, 0.0))
@@ -172,6 +207,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor(undefName = "*").width should be (4)
     model.histExtractor(undefName = "*").names.range.toSeq should beEqSeq(
       Seq("v=a","v=b","v=c","v=*"))
+    model.histExtractor(undefName = "*").categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor(undefName = "*").categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
 
     model.histExtractor(minFreq = 2)(Seq("a")) should beEqSeq(Seq(1.0, 0.0))
     model.histExtractor(minFreq = 2)(Seq("b")) should beEqSeq(Seq(0.0, 1.0))
@@ -180,6 +217,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor(minFreq = 2)(Seq("a", "c", "a", "b")) should beEqSeq(Seq(2.0, 1.0))
     model.histExtractor(minFreq = 2).width should be (2)
     model.histExtractor(minFreq = 2).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.histExtractor(minFreq = 2).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor(minFreq = 2).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
 
     model.histExtractor(maxFreq = 2)(Seq("a")) should beEqSeq(Seq(0.0, 0.0))
     model.histExtractor(maxFreq = 2)(Seq("b")) should beEqSeq(Seq(1.0, 0.0))
@@ -188,6 +227,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor(maxFreq = 2)(Seq("a", "c", "b", "c")) should beEqSeq(Seq(1.0, 2.0))
     model.histExtractor(maxFreq = 2).width should be (2)
     model.histExtractor(maxFreq = 2).names.range.toSeq should beEqSeq(Seq("v=b","v=c"))
+    model.histExtractor(maxFreq = 2).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor(maxFreq = 2).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
 
     model.histExtractor(minProb = 0.33)(Seq("a")) should beEqSeq(Seq(1.0, 0.0))
     model.histExtractor(minProb = 0.33)(Seq("b")) should beEqSeq(Seq(0.0, 1.0))
@@ -196,6 +237,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor(minProb = 0.33)(Seq("b", "c", "b", "a")) should beEqSeq(Seq(1.0, 2.0))
     model.histExtractor(minProb = 0.33).width should be (2)
     model.histExtractor(minProb = 0.33).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.histExtractor(minProb = 0.33).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor(minProb = 0.33).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
 
     model.histExtractor(maxProb = 0.34)(Seq("a")) should beEqSeq(Seq(0.0, 0.0))
     model.histExtractor(maxProb = 0.34)(Seq("b")) should beEqSeq(Seq(1.0, 0.0))
@@ -204,6 +247,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor(maxProb = 0.34)(Seq("a", "c", "b", "c")) should beEqSeq(Seq(1.0, 2.0))
     model.histExtractor(maxProb = 0.34).width should be (2)
     model.histExtractor(maxProb = 0.34).names.range.toSeq should beEqSeq(Seq("v=b","v=c"))
+    model.histExtractor(maxProb = 0.34).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor(maxProb = 0.34).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
 
     model.histExtractor(maxSize = 2)(Seq("a")) should beEqSeq(Seq(1.0, 0.0))
     model.histExtractor(maxSize = 2)(Seq("b")) should beEqSeq(Seq(0.0, 1.0))
@@ -212,6 +257,8 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor(maxSize = 2)(Seq("a", "c", "a", "b")) should beEqSeq(Seq(2.0, 1.0))
     model.histExtractor(maxSize = 2).width should be (2)
     model.histExtractor(maxSize = 2).names.range.toSeq should beEqSeq(Seq("v=a","v=b"))
+    model.histExtractor(maxSize = 2).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor(maxSize = 2).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
 
     model.histExtractor(minFreq = 4)(Seq("a")) should beEqSeq(Seq.empty[Double])
     model.histExtractor(maxFreq = 0)(Seq("a")) should beEqSeq(Seq.empty[Double])
@@ -220,5 +267,7 @@ class OneHotModelSpec extends FlatSpec with Matchers {
     model.histExtractor(maxSize = 0)(Seq("a")) should beEqSeq(Seq.empty[Double])
     model.histExtractor(maxSize = 0).width should be (0)
     model.histExtractor(maxSize = 0).names.range.toSeq should beEqSeq(Seq[String]())
+    model.histExtractor(maxSize = 0).categoryInfo.domain.toSeq should beEqSeq(Seq[Int]())
+    model.histExtractor(maxSize = 0).categoryInfo.range.toSeq should beEqSeq(Seq[Int]())
   }
 }
