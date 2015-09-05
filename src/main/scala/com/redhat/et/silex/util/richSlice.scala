bfc833786da254f91a4da1209d751f98ee913264
Erik Erlandson <eerlands@redhat.com>
2015-09-04 17:55:03 -0700

    documentation

--- src/main/scala/com/redhat/et/silex/util/richSlice.scala
+++ src/main/scala/com/redhat/et/silex/util/richSlice.scala
@@ -18,6 +18,23 @@
 
 package com.redhat.et.silex.util
 
+/**
+  * @define rsExamples {{{
+  * import com.redhat.et.silex.util.richslice._
+  * RichSlice(1)             // selects element(1)
+  * RichSlice(2, 3, 5 to 11) // selects elements 2, 3 and 5 through 11
+  * RichSlice(9 to 0 by -1)  // selects first 10 elements in reverse order
+  * RichSlice(20 to * by 2)  // selects elements 20 to end, by 2s
+  * }}}
+  *
+  * @define rsStarExamples {{{
+  * import com.redhat.et.silex.util.richslice._
+  * data.richSlice(* to 5)   // selects elements 0 through 5
+  * data.richSlice(5 to *)   // selects elmeents 5 up to end of data
+  * data.richSlice(*)        // selects all data
+  * data.richSlice(* by -1)  // selects data in reverse order
+  * }}}
+  */
 object richslice {
   import scala.language.higherKinds
   import scala.collection.SeqLike
@@ -25,6 +42,7 @@ object richslice {
   import scala.language.implicitConversions
   import scala.collection.immutable.Range
 
+  /** Internal mechanism to support use of '*' in rich slice expressions as a wildcard index */
   sealed trait Star {
     def to(j: Int): Range = starIdx to j
     def to(s: Star): Range = starIdx to starIdx
@@ -32,15 +50,24 @@ object richslice {
     def until(s: Star): Range = starIdx until starIdx
     def by(step: Int): Range = starIdx until starIdx by step
   }
+
+  /** Internal mechanism to support use of '*' in rich slice expressions as a wildcard index */
   object star extends Star
-  def * = star
   private val starIdx = scala.Int.MinValue
 
+  /** Internal mechanism to support use of '*' in rich slice expressions as a wildcard index */
   implicit class StarToRange(n: Int) {
     def to(s: Star): Range = n to starIdx
     def until(s: Star): Range = n until starIdx
   }
 
+  /** When '*' is used in place of a numeric index, it matches all indices up to or after a 
+    * bound.
+    * $rsStarExamples
+    */
+  def * = star
+
+  /** One slice in a rich slice expression list */
   sealed trait Slice
   private case class IndexS(idx: Int) extends Slice
   private case class RangeS(beg: Int, end: Int) extends Slice
@@ -48,6 +75,7 @@ object richslice {
     require(step != 0, "RichSlice step values cannot be zero")
   }
 
+  /** Implicit conversions from rich slice expressions to Slice objects */
   object Slice {
     implicit def intToSlice(idx: Int): Slice = IndexS(idx)
 
@@ -59,18 +87,47 @@ object richslice {
     implicit def starToSlice(s: Star): Slice = RangeS(starIdx, starIdx)
   }
 
+  /** A Rich Slice object, containing a sequence of slice expressions 
+    * $rsExamples
+    */
   case class RichSlice(slices: List[Slice]) extends Serializable
 
+  /** Factory methods for instantiating RichSlice objects 
+    * $rsExamples
+     */
   object RichSlice {
+    /** Create a RichSlice object from zero or more individual slice expressions.  RichSlice objects
+      * can be created and then given to the rich slice enhanced Seq methods.
+      * $rsExamples
+      */
     def apply(slices: Slice*): RichSlice = RichSlice(slices.toList)
   }
 
+  /** Provides rich slice methods on Scala Seq types */
   implicit class RichSliceMethods[A, S[E] <: SeqLike[E, S[E]]](seq: S[A]) {
     private val N = seq.length
 
+    /** Create an iterator that iterates over the sequence elements selcted by zero or more
+      * slice expresions.
+      * {{{
+      * import com.redhat.et.silex.util.richslice._
+      * data.richSliceIterator(1)             // iterate over data(1)
+      * data.richSliceIterator(2, 3, 5 to 11) // over data(2), data(3) and data(5) through data(11)
+      * data.richSliceIterator(9 to 0 by -1)  // over 1st 10 elements of data in reverse
+      * data.richSliceIterator(20 to * by 2)  // over data(20) to end, by 2s
+      * }}}
+      */
     def richSliceIterator(slices: Slice*): Iterator[A] =
       richSliceIterator(RichSlice(slices.toList))
 
+    /** Create an iterator that iterates over the sequence elements selected by the given
+      * rich slice object
+      * {{{
+      * import com.redhat.et.silex.util.richslice._
+      * val rs = RichSlice(2, 3, 5 to 11)
+      * data.richSliceIterator(rs) // iterate over data(2), data(3) and data(5) through data(11)
+      * }}}
+      */
     def richSliceIterator(rs: RichSlice): Iterator[A] = {
       val itrList = rs.slices.map(resolveNegative).map {
         case IndexS(j) => Iterator.single(seq(j))
@@ -102,12 +159,30 @@ object richslice {
       else itrList.fold(Iterator.empty)(_ ++ _)
     }
 
+    /** Create a new sub sequence from the elements selcted by zero or more
+      * slice expresions.
+      * {{{
+      * import com.redhat.et.silex.util.richslice._
+      * data.richSlice(1)             // select data(1)
+      * data.richSlice(2, 3, 5 to 11) // select data(2), data(3) and data(5) through data(11)
+      * data.richSlice(9 to 0 by -1)  // select 1st 10 elements of data in reverse
+      * data.richSlice(20 to * by 2)  // select data(20) to end, by 2s
+      * }}}
+      */
     def richSlice(slices: Slice*)(implicit cbf: CanBuildFrom[Nothing, A, S[A]]): S[A] =
       richSliceIterator(RichSlice(slices.toList)).to[S]
 
+    /** Create a new subsequence from the elmenents selected by a given rich slice object
+      * {{{
+      * import com.redhat.et.silex.util.richslice._
+      * val rs = RichSlice(2, 3, 5 to 11)
+      * data.richSlice(rs) // select data(2), data(3) and data(5) through data(11)
+      * }}}
+      */
     def richSlice(rs: RichSlice)(implicit cbf: CanBuildFrom[Nothing, A, S[A]]): S[A] =
       richSliceIterator(rs).to[S]
 
+    /** Used internally by rich slice iterators */
     class IterU(data: S[A], beg: Int, end: Int, step: Int) extends Iterator[A] {
       require(step > 0)
       var j = if (beg < end) beg else end
@@ -118,6 +193,8 @@ object richslice {
         r
       }
     }
+
+    /** Used internally by rich slice iterators */
     class IterD(data: S[A], beg: Int, end: Int, step: Int) extends Iterator[A] {
       require(step < 0)
       var j = if (beg > end) beg else end
