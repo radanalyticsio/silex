07d48f8f7e783eefff1b6d7b6500c7054c0accde
Erik Erlandson <eerlands@redhat.com>
2016-02-10 11:22:34 -0700

    ancestry --> lineage

--- src/main/scala/com/redhat/et/silex/rdd/ancestry.scala
+++ src/main/scala/com/redhat/et/silex/rdd/ancestry.scala
@@ -16,7 +16,7 @@
  * limitations under the License
  */
 
-package com.redhat.et.silex.rdd.ancestry
+package com.redhat.et.silex.rdd.lineage
 
 import scala.reflect.ClassTag
 
@@ -24,25 +24,25 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.Logging
 
 /**
- * Encodes an ancestor in the dependency lineage of a target RDD.
- * @param rdd The ancestor RDD itself
- * @param ply The ply, or depth, of this RDD in the lineage.  What is the path length from
- * the target RDD to this RDD, in the orginal's lineage?
- * @param child The RDD whose dependency this RDD represents.
- * @note It is possible for an RDD to be reachable along multiple lineage paths from the target,
- * and so one ancestor RDD may appear multiple times in a target RDD's ancestry.
+ * Encodes an RDD dependency arc in the lineage DAG of a target RDD.
+ * @param rdd The RDD dependency of the target
+ * @param depth The depth along the current path from the target RDD to this dependency.
+ * It is possible for an RDD to be reachable along multiple paths starting from the target.
+ * Therefore one dependency RDD may appear multiple times in the lineage, with differing values
+ * for depth depending on the particular path.
+ * @param successor The RDD whose immediate dependency this is.
  */
-class RDDAncestor(val rdd: RDD[_], val ply: Int, val child: RDD[_])
+class Dependency(val rdd: RDD[_], val depth: Int, val successor: RDD[_])
 
-/** Enhances RDDs with ancestry methods */
-class AncestryRDDFunctions[T :ClassTag](self: RDD[T]) extends Logging with Serializable {
+/** Enhances RDDs with lineage methods */
+class LineageRDDFunctions[T :ClassTag](self: RDD[T]) extends Logging with Serializable {
 
   /**
-   * Obtain an iterator over an RDD's ancestors in it's dependency lineage.  Iteration is
+   * Obtain an iterator over an RDD's dependencies in its lineage DAG.  Iteration is
    * in breadth-first order.
-   * @return An iterator over the RDD's lineage ancestors.
+   * @return An iterator over the RDD's dependency lineage.
    */
-  def ancestry = new Iterator[RDDAncestor] {
+  def lineage = new Iterator[Dependency] {
     import scala.collection.mutable.Queue
 
     private val q = Queue(parents(self, 1) :_*)
@@ -51,18 +51,18 @@ class AncestryRDDFunctions[T :ClassTag](self: RDD[T]) extends Logging with Seria
 
     def next = {
       val n = q.dequeue()
-      q.enqueue(parents(n.rdd, n.ply + 1) :_*)
+      q.enqueue(parents(n.rdd, n.depth + 1) :_*)
       n
     }
 
-    private def parents(rdd: RDD[_], ply: Int) =
-      rdd.dependencies.map(_.rdd).map { r => new RDDAncestor(r, ply, rdd) }
+    private def parents(rdd: RDD[_], depth: Int) =
+      rdd.dependencies.map(_.rdd).map { r => new Dependency(r, depth, rdd) }
   }
 }
 
-/** Implicit conversions to enhance RDDs with ancestry methods */
+/** Implicit conversions to enhance RDDs with lineage methods */
 object implicits {
   import scala.language.implicitConversions
-  implicit def fromRDD[T :ClassTag](rdd: RDD[T]): AncestryRDDFunctions[T] =
-    new AncestryRDDFunctions(rdd)
+  implicit def toLineageRDDFunctions[T :ClassTag](rdd: RDD[T]): LineageRDDFunctions[T] =
+    new LineageRDDFunctions(rdd)
 }
