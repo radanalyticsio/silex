7f54e3a74e8cec1db73de14f36223da29f9796dd
RJ Nowling <rnowling@gmail.com>
2016-03-29 06:12:46 -0500

    Provide implementation of Cramer's V.
    Closes issue #52.

---
+++ src/main/scala/com/redhat/et/silex/statistics/cramersv.scala
@@ -0,0 +1,120 @@
+/*
+ * cramersv.scala
+ * author:  RJ Nowling <rnowling@redhat.com>
+ *
+ * Copyright (c) 2016 Red Hat, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.redhat.et.silex.statistics
+
+import scala.util.Random
+
+/**
+ * "[[https://en.wikipedia.org/wiki/Cram%C3%A9r%27s_V Cramers' V]] is a measure of
+ * association between two nominal variables, giving a value between 0 and +1
+ * (inclusive)." 
+ */
+object CramersV {
+  /**
+   * Calculate Cramer's V for two sets (`values1` and `values2`) of co-sampled values.
+   */
+  def apply[T, U](values1 : Seq[T], values2 : Seq[U]) : Double = {
+    val set1 = values1.toSet
+    val set2 = values2.toSet
+
+    if (set1.size == 1 && set2.size == 1) {
+      1.0
+    } else if (values1.size == 0 || set1.size == 1 || set2.size == 1) {
+      0.0
+    } else {
+      val pairCounts = values1.zip(values2)
+        .foldLeft(Map.empty[(T, U), Double]) {
+        case (counts, value) =>
+          counts + (value -> (counts.getOrElse(value, 0.0) + 1.0))
+      }
+
+      val counts1 = values1
+        .foldLeft(Map.empty[T, Double]) {
+        case (counts, value) =>
+          counts + (value -> (counts.getOrElse(value, 0.0) + 1.0))
+      }
+
+      val counts2 = values2
+        .foldLeft(Map.empty[U, Double]) {
+        case (counts, value) =>
+          counts + (value -> (counts.getOrElse(value, 0.0) + 1.0))
+      }
+
+      val nObs = values1.size.toDouble
+
+      val combinations = set1.flatMap {
+        v1 =>
+          set2.map {
+            v2 =>
+              (v1, v2)
+          }
+      }
+      .toSeq
+
+      val chi2 = combinations.map {
+        case (value1, value2) =>
+          val nij = pairCounts.getOrElse((value1, value2), 0.0)
+          val ni = counts1.getOrElse(value1, 0.0)
+          val nj = counts2.getOrElse(value2, 0.0)
+
+          val b = ni * nj / nObs
+          val c = (nij - b) * (nij - b) / b
+
+          c / nObs
+      }
+      .sum
+
+      val minDim = math.min(set1.size - 1, set1.size - 1).toDouble
+      
+      val v = math.sqrt(chi2 / minDim)
+
+      v
+    }
+  }
+
+  /**
+   * Perform a permutation test to get a p-value indicating the probability of getting
+   * a lower assocation value.  Take the association level as the null hypothesis, reject
+   * if the p-value is less than your desired threshold.
+   *
+   * The parameter `rounds` indicates how many permutations to generate. A random number
+   * generator can be provided (if desired) via the `rng` parameter.
+   */
+  def permutationTest[T, U](values1 : Seq[T], values2 : Seq[U], rounds : Int, rng : Random = new Random()) : Double = {
+//    val rng = new Random()
+
+    val testV = CramersV(values1, values2)
+    
+    val worseCount = (1 to rounds).map {
+      i =>
+        val shuffled = rng.shuffle(values1)
+        CramersV(shuffled, values2)
+    }
+    .filter {
+      v =>
+        v < testV
+    }
+    .size
+
+    val pvalue = worseCount.toDouble / rounds.toDouble
+
+    pvalue
+  }
+}
---
+++ src/test/scala/com/redhat/et/silex/statistics/cramsersv.scala
@@ -0,0 +1,88 @@
+/*
+ * cramersv.scala
+ * author:  RJ Nowling <rnowling@redhat.com>
+ *
+ * Copyright (c) 2016 Red Hat, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.redhat.et.silex.statistics
+
+import com.redhat.et.silex.testing.PerTestSparkContext
+
+import org.scalatest._
+
+class CramersVSpec extends FlatSpec with Matchers with PerTestSparkContext {
+
+  val eps = 1e-5
+
+  "CramersV.apply" should "report 1.0 under perfect association" in {
+    val values1 = Seq(1, 1, 1, 0, 0, 0, 2, 2, 2)
+    val values2 = Seq(0, 0, 0, 1, 1, 1, 2, 2, 2)
+    val values3 = Seq(1, 1, 1, 1, 2, 2, 1, 1)
+    val values4 = Seq(3, 3, 3, 3, 4, 4, 3, 3)
+
+    val sameV12 = CramersV(values1, values1)
+    val negatedV12 = CramersV(values1, values2)
+    val v34 = CramersV(values3, values4)
+
+    assert(math.abs(sameV12 - 1.0) < eps)
+    assert(math.abs(negatedV12 - 1.0) < eps)
+    assert(math.abs(v34 - 1.0) < eps)
+  }
+
+  "CramersV.apply" should "report 0.0 with no association" in {
+    val values1 = Seq(1, 1, 1, 1, 0, 0, 0, 0)
+    val values2 = Seq(0, 1, 0, 1, 1, 0, 1, 0)
+ 
+    val v = CramersV(values1, values2)
+
+    assert(v < eps) 
+  }
+
+  "CramersV.apply" should "report 1.0 with single-value sets" in {
+    val values1 = Seq(1, 1, 1, 1)
+    val values2 = Seq(1, 1, 1, 1)
+
+    val v = CramersV(values1, values2)
+
+    assert(math.abs(v - 1.0) < eps)
+  }
+
+  "CramersV.apply" should "report 0.0 when one variable has 1 value and the other has more" in {
+    val values1 = Seq(1, 1, 1, 1)
+    val values2 = Seq(1, 2, 3, 4)
+
+    val v = CramersV(values1, values2)
+
+    assert(v < eps)
+  }
+
+  "CramersV.apply" should "report 0.0 for empty sets" in {
+    val v = CramersV(Seq[Int](), Seq[Int]())
+
+    assert(v < eps)
+  }
+
+  "CramersV.permutationTest" should "report p-value 1.0 under perfect correlation" in {
+    val values = (1 to 100).flatMap {
+      i =>
+        Seq(i, i)
+    }
+    
+    val pvalue = CramersV.permutationTest(values, values, 1000)
+
+    assert(math.abs(pvalue - 1.0) < eps)
+  }
+}
